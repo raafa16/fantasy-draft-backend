0f4f770380a6911e33115861f59729fbd133240e
raafa16 <raafa16@hotmail.com>
2020-03-14 01:38:36 -0400

    add model validations

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -3,5 +3,9 @@ class Draft < ApplicationRecord
   has_and_belongs_to_many :players, class_name: 'User'
   belongs_to :manager, class_name: 'User', foreign_key: 'user_id'
 
-  attr_accessible :, :manager
+  attr_accessible :name, :manager
+
+  # Validations
+  validates :name, presence: true, uniqueness: true
+  validates :manager, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_and_belongs_to_many :drafts
   has_many :managed_drafts, class_name: 'Draft', foreign_key: 'user_id'
   attr_accessible :name, :email
+
+  # Validations
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
