871cd324e42a44f7551cc1bcfea1df8cbbcebfc5
raafa16 <raafa16@hotmail.com>
2020-04-04 06:46:00 -0400

    fix block

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,16 +2,16 @@ module CurrentUserConcern
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_current_user
+    before_action :current_user
+  end
 
-    def set_current_user
-      return @current_user if defined?(@current_user)
-      @current_user = User.find_by(id: session[:id]) if session[:user_id]
-    end
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = User.find_by(id: session[:id]) if session[:user_id]
+  end
 
-    def sign_in(user)
-      user_id = user.respond_to?(:id) ? user.id : user
-      session[:id] = user_id
-    end
+  def sign_in(user)
+    user_id = user.respond_to?(:id) ? user.id : user
+    session[:id] = user_id
   end
 end
